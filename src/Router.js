4aa58a5b088755ecedf67dfd07a36fa3ba1c0e54
parth <marlabsjsbatch@gmail.com>
2017-08-04 09:34:02 -0400

    second commit

--- src/Router.js
+++ src/Router.js
@@ -3,7 +3,6 @@ import { Router, Route, browserHistory} from "react-router";
 
 import HomePage from "./components/HomePage/HomePage";
 import AboutPage from "./components/AboutPage/AboutPage";
-import ManageAuthorPage from "./components/Authors/manageAuthorPage";
 
 class AppRouter extends React.Component {
         render(){
@@ -11,7 +10,6 @@ class AppRouter extends React.Component {
                         <Router history={browserHistory}>
                                 <Route path="/" component={HomePage}></Route>
                                 <Route path="/about" component={AboutPage}></Route>
-                                <Route path="/author" component={ManageAuthorPage}></Route>
                         </Router>
                 );
         }
--- src/header/Header.js
+++ src/header/Header.js
@@ -19,7 +19,6 @@ class Header extends React.Component {
                             <ul className="nav navbar-nav">
                                 <li><Link to="/">Home</Link></li>
                                 <li><Link to="/about">About</Link></li>
-                                <li><Link to="/author">Author</Link></li>
                             </ul>
                         </div>
                     </div>
